04096d37ff59a44e6d4d75ede1df3358046e64cb
SilverLóz <slpezromano.sl@gmail.com>
2025-06-09 09:04:14 -0600

    Integración de mapa y contacto por whatsapp

---
+++ js/contacto.js
@@ -0,0 +1,6 @@
+function sinCoverturaContacto() {
+  window.open(
+    "https://wa.me/+525951214378?text=Hola,%20vengo%20desde%20la%20p%C3%A1gina%20web%20El%20gran%20Mes%C3%B3n,%20quisiera%20consultar%20lo%20siguiente......",
+    "_blank"
+  );
+}
---
+++ js/index.js
@@ -0,0 +1,35 @@
+/* MAP CANVAS*/
+function initialize() {
+    var lat = 19.468721;
+    var lon = -97.926971;
+    var myLatLng = new google.maps.LatLng(lat, lon);
+    var mapOptions = {
+      zoom: 19,
+      center: myLatLng,
+      mapTypeId: google.maps.MapTypeId.SATELLITE,
+      disableDefaultUI: true,
+      zoomControl: true,
+      mapTypeControl: true,
+      scaleControl: false,
+      streetViewControl: false,
+      rotateControl: true,
+      fullscreenControl: false,
+    };
+      
+    var map = new google.maps.Map(
+      document.getElementById("map-canvas"),
+      mapOptions
+    );
+
+    var marker = new google.maps.Marker({
+        position: myLatLng,
+        map: map,
+        title: "Ubicación central",
+        icon: {
+          //url: "http://maps.google.com/mapfiles/ms/icons/red-dot.png"
+            url: "../img/alfiler.png", // Reemplaza por tu URL o ruta local
+            scaledSize: new google.maps.Size(40, 40) // Ajusta tamaño si es necesario
+        }
+      });
+      
+}
