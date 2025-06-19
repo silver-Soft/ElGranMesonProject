2a8572a829230c4cbbfc16898068991be50b45ac
SilverLóz <slpezromano.sl@gmail.com>
2025-06-19 16:24:37 -0600

    Nueva BD

--- bd/gran_meson.sql
+++ bd/gran_meson.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 10-06-2025 a las 04:26:19
--- Versión del servidor: 10.4.32-MariaDB
--- Versión de PHP: 8.0.30
+-- Tiempo de generación: 20-06-2025 a las 00:23:30
+-- Versión del servidor: 10.4.25-MariaDB
+-- Versión de PHP: 7.4.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,12 +24,108 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `estatus_reservacion`
+--
+
+CREATE TABLE `estatus_reservacion` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `estatus_reservacion`
+--
+
+INSERT INTO `estatus_reservacion` (`id`, `nombre`) VALUES
+(1, 'Activa'),
+(2, 'Finalizada'),
+(3, 'Cancelada');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `horarios`
+--
+
+CREATE TABLE `horarios` (
+  `id_horario` int(11) NOT NULL,
+  `horario` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `horarios`
+--
+
+INSERT INTO `horarios` (`id_horario`, `horario`) VALUES
+(1, '12:00 - 14:00'),
+(2, '14:00 - 16:00'),
+(3, '16:00 - 18:00'),
+(4, '18:00 - 20:00'),
+(5, '20:00 - 22:00'),
+(6, '22:00 - 00:00');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `ordenes`
+--
+
+CREATE TABLE `ordenes` (
+  `id_orden` int(11) NOT NULL,
+  `id_reservacion` int(11) NOT NULL,
+  `monto` decimal(10,2) NOT NULL,
+  `metodo_pago` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `fecha_pago` datetime DEFAULT current_timestamp(),
+  `estatus_pago` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `referencia_externa` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `ordenes`
+--
+
+INSERT INTO `ordenes` (`id_orden`, `id_reservacion`, `monto`, `metodo_pago`, `fecha_pago`, `estatus_pago`, `referencia_externa`) VALUES
+(1, 6, '750.00', 'Tarjeta', '2025-06-18 12:15:10', 'exitoso', 'sandbox-123456'),
+(4, 11, '700.00', 'Tarjeta de prueba', '2025-06-18 15:17:11', 'exitoso', 'sandbox-68532cd730a38'),
+(5, 12, '800.00', 'Tarjeta de prueba', '2025-06-19 15:30:58', 'exitoso', 'sandbox-68548192797c1');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `reservaciones`
+--
+
+CREATE TABLE `reservaciones` (
+  `id_reservacion` int(11) NOT NULL,
+  `id_usuario` int(11) NOT NULL,
+  `num_personas` int(11) NOT NULL,
+  `fecha` date NOT NULL,
+  `id_horario` int(11) NOT NULL,
+  `con_mascotas` tinyint(1) DEFAULT 0,
+  `num_mascotas` int(11) DEFAULT NULL,
+  `id_estatus` int(11) NOT NULL DEFAULT 1,
+  `id_orden` int(11) DEFAULT NULL,
+  `fecha_registro` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `reservaciones`
+--
+
+INSERT INTO `reservaciones` (`id_reservacion`, `id_usuario`, `num_personas`, `fecha`, `id_horario`, `con_mascotas`, `num_mascotas`, `id_estatus`, `id_orden`, `fecha_registro`) VALUES
+(6, 2, 3, '2025-06-18', 2, 1, 1, 3, 1, '2025-06-18 18:14:02'),
+(11, 2, 2, '2025-06-25', 2, 1, 2, 3, 4, '2025-06-18 21:17:07'),
+(12, 2, 3, '2025-06-26', 2, 1, 3, 2, 5, '2025-06-19 21:30:44');
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `roles`
 --
 
 CREATE TABLE `roles` (
   `id_rol` int(11) NOT NULL,
-  `descrip` varchar(50) NOT NULL
+  `descrip` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
@@ -41,7 +137,8 @@ INSERT INTO `roles` (`id_rol`, `descrip`) VALUES
 (2, 'admin'),
 (3, 'supervisor'),
 (4, 'capitan'),
-(5, 'camarero');
+(5, 'camarero'),
+(10, 'cliente');
 
 -- --------------------------------------------------------
 
@@ -50,27 +147,55 @@ INSERT INTO `roles` (`id_rol`, `descrip`) VALUES
 --
 
 CREATE TABLE `usuarios` (
-  `id` int(11) NOT NULL,
-  `usuario` varchar(50) NOT NULL,
-  `contrasena` varchar(255) NOT NULL,
-  `nombre` varchar(100) NOT NULL,
-  `apellidos` varchar(100) NOT NULL,
-  `id_rol` int(10) DEFAULT NULL,
-  `rol` varchar(50) DEFAULT NULL
+  `id_usuario` int(11) NOT NULL,
+  `usuario` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `contrasena` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `nombre` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `apellidos` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `telefono` varchar(20) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `id_rol` int(10) NOT NULL DEFAULT 10,
+  `rol` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Volcado de datos para la tabla `usuarios`
 --
 
-INSERT INTO `usuarios` (`id`, `usuario`, `contrasena`, `nombre`, `apellidos`, `id_rol`, `rol`) VALUES
-(1, 'Marely', '$2y$10$ylSYPMzwVmijNSjI.XwdquHsuifGu4obn4Fy4zGbdrzGT6i5EZDtW', 'Marely', 'Romero', 1, 'root'),
-(2, 'Jennfriki', '$2y$10$2KHjRDUj97kY0nIpoUmlCe0up4V2VjXd0XVFPyR7hi0YDuBdOPHQ6', 'Jennyfer', 'Romero López', 2, 'admin'),
-(3, 'uripakini', '$2y$10$gR2ZOWVu0V4PVJMRVdUor.Auz6Gxmj0FgY/aJnkcXuPZuxHU0PL3S', 'Uriel ', 'Pakini', 3, 'supervisor');
+INSERT INTO `usuarios` (`id_usuario`, `usuario`, `contrasena`, `nombre`, `apellidos`, `telefono`, `id_rol`, `rol`) VALUES
+(1, 'Marely', '$2y$10$ylSYPMzwVmijNSjI.XwdquHsuifGu4obn4Fy4zGbdrzGT6i5EZDtW', 'Marely', 'Romero', NULL, 1, 'root'),
+(2, 'slpezromano.sl@gmail.com', '$2y$10$.mwe8pemChHoV//vboCdR.IZvoWiQWUBsxKWQeAUuupxRNfR0pazu', 'Joaquín ', 'López Dóriga', NULL, 10, 'cliente');
 
 --
 -- Índices para tablas volcadas
 --
+
+--
+-- Indices de la tabla `estatus_reservacion`
+--
+ALTER TABLE `estatus_reservacion`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `horarios`
+--
+ALTER TABLE `horarios`
+  ADD PRIMARY KEY (`id_horario`);
+
+--
+-- Indices de la tabla `ordenes`
+--
+ALTER TABLE `ordenes`
+  ADD PRIMARY KEY (`id_orden`),
+  ADD KEY `id_reservacion` (`id_reservacion`);
+
+--
+-- Indices de la tabla `reservaciones`
+--
+ALTER TABLE `reservaciones`
+  ADD PRIMARY KEY (`id_reservacion`),
+  ADD KEY `id_usuario` (`id_usuario`),
+  ADD KEY `id_horario` (`id_horario`),
+  ADD KEY `id_estatus` (`id_estatus`);
 
 --
 -- Indices de la tabla `roles`
@@ -82,7 +207,7 @@ ALTER TABLE `roles`
 -- Indices de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  ADD PRIMARY KEY (`id`),
+  ADD PRIMARY KEY (`id_usuario`),
   ADD UNIQUE KEY `usuario` (`usuario`);
 
 --
@@ -90,16 +215,58 @@ ALTER TABLE `usuarios`
 --
 
 --
+-- AUTO_INCREMENT de la tabla `estatus_reservacion`
+--
+ALTER TABLE `estatus_reservacion`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `horarios`
+--
+ALTER TABLE `horarios`
+  MODIFY `id_horario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT de la tabla `ordenes`
+--
+ALTER TABLE `ordenes`
+  MODIFY `id_orden` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT de la tabla `reservaciones`
+--
+ALTER TABLE `reservaciones`
+  MODIFY `id_reservacion` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
 -- AUTO_INCREMENT de la tabla `roles`
 --
 ALTER TABLE `roles`
-  MODIFY `id_rol` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id_rol` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `ordenes`
+--
+ALTER TABLE `ordenes`
+  ADD CONSTRAINT `ordenes_ibfk_1` FOREIGN KEY (`id_reservacion`) REFERENCES `reservaciones` (`id_reservacion`);
+
+--
+-- Filtros para la tabla `reservaciones`
+--
+ALTER TABLE `reservaciones`
+  ADD CONSTRAINT `reservaciones_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id_usuario`),
+  ADD CONSTRAINT `reservaciones_ibfk_2` FOREIGN KEY (`id_horario`) REFERENCES `horarios` (`id_horario`),
+  ADD CONSTRAINT `reservaciones_ibfk_3` FOREIGN KEY (`id_estatus`) REFERENCES `estatus_reservacion` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
